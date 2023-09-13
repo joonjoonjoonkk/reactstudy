7aeae28744026cee8585bbf61d74e51abddc708f
sianjoon66 <sianjoon66@naver.com>
2023-09-13 21:16:05 +0900

    chapter_0.3

---
+++ src/chapter_0.3/Library.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Book from "./Book";
+
+function Library(props){
+    return(
+        <div>
+            <Book name='책1번' numOfPage = {300}/>
+            <Book name='책2번' numOfPage = {400}/>
+            <Book name='책3번' numOfPage = {500}/>
+        </div>
+    )
+}
+
+export default  Library;
