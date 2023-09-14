75c16da83cfb105aa5d00af3ebbd50e27d75f461
sianjoon66 <sianjoon66@naver.com>
2023-09-14 20:36:14 +0900

    crel

---
+++ src/chapter_0.3/createElement.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function CreateElement(params){
+    return(
+        React.CreateElement(
+            
+        )
+    )
+}
+
+export default CreateElement;
+
+/* <div>
+            <div>
+                <h1>주제</h1>
+                <ul>
+                    <l1>React</l1>
+                    <l1>Vue</l1>
+                </ul>
+            </div>
+        </div> */
